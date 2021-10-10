5404d2c799123eb3f5f358f98432bb6b77b846ab
uolamer <91064255+uolamer@users.noreply.github.com>
2021-10-10 08:28:22 -0500

    Delete Remote-Tools directory

--- Remote-Tools/Remote-Tools.psm1
+++
@@ -1,5 +0,0 @@
-. $psScriptRoot\Get-RemoteService.ps1
-. $psScriptRoot\Stop-RemoteService.ps1
-. $psScriptRoot\Start-RemoteService.ps1
-. $psScriptRoot\Restart-RemoteService.ps1
-. $psScriptRoot\Start-Timeout.ps1
